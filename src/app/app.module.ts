4f77baa68f203614b5b6d048019e3a00c4e27579
Alexandr Plokhih <s.plohoj@gmail.com>
2018-01-10 22:47:59 +0300

    Download friends IDs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { SearchComponent } from "./pages/search/search.component";
+import { PersonStorageService } from "./servises/person.storage.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, SearchComponent],
+	imports: [BrowserModule],
+	providers: [PersonStorageService],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PersonStorageService, Person } from '../../servises/person.storage.service';
+
+@Component({
+	selector: 'app-search',
+	templateUrl: './search.component.html',
+	styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+	person: Person;
+	constructor(private personStorageService: PersonStorageService) {
+		this.person = this.personStorageService.get(293423171);
+		this.person.loadFriends();
+	}
+
+	ngOnInit() {
+
+	}
+}
+function createPerson(id){ return new Person(id) };
+}
